de02c75df70a7ea0bbf8c5477f823d610a27186d
Baily Troyer <bailytroyer@gmail.com>
2022-10-16 20:44:28 -0400

    initial app with i18 support

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import astroI18next from "astro-i18next";
+
+export default defineConfig({
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "en", // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en", // The `defaultLocale` value must present in `locales` keys
+          fr: "fr",
+        },
+      },
+    }),
+    astroI18next(),
+  ],
+});
